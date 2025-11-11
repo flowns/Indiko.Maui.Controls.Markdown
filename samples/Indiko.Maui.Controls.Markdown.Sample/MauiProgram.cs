d1efd9c7f27c8a427bfef4edd124f9934878391a
Daniel Bergström <daniel@bergstrom.at>
2025-11-11 08:36:44 +0100

    Removed unused fonts

--- samples/Indiko.Maui.Controls.Markdown.Sample/MauiProgram.cs
+++ samples/Indiko.Maui.Controls.Markdown.Sample/MauiProgram.cs
@@ -3,26 +3,6 @@
 namespace Indiko.Maui.Controls.Markdown.Sample;
 public static class MauiProgram
 {
-    public static class FontAlias
-    {
-        public const string BrandonBold = "BrandonBold";
-        public const string BrandonRegular = "BrandonRegular";
-        public const string OpenSansBold = "OpenSansBold";
-        public const string OpenSansItalic = "OpenSansItalic";
-        public const string OpenSansRegular = "OpenSansRegular";
-        public const string OpenSansSemiBold = "OpenSansSemiBold";
-    }
-    
-    public static readonly Dictionary<string, string> FontFiles = new()
-    {
-        { FontAlias.BrandonBold, "neueSerie57®-Medium.otf" },
-        { FontAlias.BrandonRegular, "neueSerie57®-Regular.otf" },
-        { FontAlias.OpenSansBold, "neueSerie57®-Medium.otf" },
-        { FontAlias.OpenSansItalic, "neueSerie57®-RegularItalic.otf" },
-        { FontAlias.OpenSansRegular, "neueSerie57®-Regular.otf" },
-        { FontAlias.OpenSansSemiBold, "neueSerie57®-Book.otf" }
-    };
-    
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
@@ -30,12 +10,9 @@ public static class MauiProgram
             .UseMarkdownView()
             .ConfigureFonts(fonts =>
         {
-            fonts.AddFont(FontFiles[FontAlias.BrandonBold], FontAlias.BrandonBold);
-            fonts.AddFont(FontFiles[FontAlias.BrandonRegular], FontAlias.BrandonRegular);
-            fonts.AddFont(FontFiles[FontAlias.OpenSansBold], FontAlias.OpenSansBold);
-            fonts.AddFont(FontFiles[FontAlias.OpenSansItalic], FontAlias.OpenSansItalic);
-            fonts.AddFont(FontFiles[FontAlias.OpenSansRegular], FontAlias.OpenSansRegular);
-            fonts.AddFont(FontFiles[FontAlias.OpenSansSemiBold], FontAlias.OpenSansSemiBold);
+            fonts.AddFont("neueSerie57®-Regular.otf", "OpenSansRegular");
+            fonts.AddFont("neueSerie57®-Book.otf", "OpenSansSemiBold");
+            fonts.AddFont("neueSerie57®-RegularItalic.otf", "OpenSansItalic");
         });
 
         builder.Services.AddSingleton<MainPage>();
